6e6974e3cf8e091a533916ec5b0296c129d02b7a
gfxmonk <tim3d.junk@gmail.com>
2010-10-27 22:24:26 +1100

    some more OO for mpris

--- irank/mpris.py
+++ irank/mpris.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 import os, sys, re, time, urllib2, dbus, gobject
 
-from dbus.mainloop.glib import DBusGMainLoop
-DBusGMainLoop(set_as_default=True)
 METADATA = 'Metadata'
 URL_PROPERTY = 'xesam:url'
 
@@ -10,25 +8,63 @@ mpris_prefix="org.mpris.MediaPlayer2."
 PLAYER = 'org.mpris.MediaPlayer2.Player'
 mpris_object="/org/mpris/MediaPlayer2"
 
-def possible_names():
+def init_glib():
+	from dbus.mainloop.glib import DBusGMainLoop
+	DBusGMainLoop(set_as_default=True)
+
+class Player(object):
+	instance = None
+	def __new__(cls, name=None):
+		if cls.instance is None:
+			init_glib()
+			instance = super(type(cls), cls).__new__(cls)
+			bus = dbus.SessionBus()
+			player_name = name or os.environ.get('MPRIS_REMOTE_PLAYER', None) or guess_player_name(bus)
+			player_namespace = mpris_prefix + player_name
+			player_obj = bus.get_object(player_namespace, mpris_object)
+
+			instance.player = dbus.Interface(player_obj, dbus_interface=PLAYER)
+			instance.properties = dbus.Interface(player_obj, dbus_interface=dbus.PROPERTIES_IFACE)
+			cls.instance = instance
+		return cls.instance
+
+	@property
+	def metadata(self):
+		return self.properties.GetAll(PLAYER)[METADATA]
+
+	@property
+	def track(self):
+		uri = self.metadata[URL_PROPERTY]
+		try:
+			return _path(uri)
+		except ValueError:
+			print "Invalid URI: %r" % (uri,)
+			raise
+
+	def each_track(self, cb):
+		def playing_uri_changed(source, properties, signature):
+			try:
+				uri = properties[METADATA][URL_PROPERTY]
+			except KeyError:
+				return
+			cb(_path(uri))
+
+		self.properties.connect_to_signal('PropertiesChanged', playing_uri_changed)
+		cb(self.track)
+		loop = gobject.MainLoop()
+		loop.run()
+
+
+def possible_names(bus):
 	return [ name[len(mpris_prefix):] for name in bus.list_names() if name.startswith(mpris_prefix) ]
 
 # returns first matching player
-def get_player():
-	names = possible_names()
+def guess_player_name(bus):
+	names = possible_names(bus)
 	if not names:
 		print >>sys.stderr, "No MPRIS-compliant player found running."
 		raise SystemExit(1)
 	return names[0]
-
-def init():
-	global bus, player_namespace, player, properties
-	bus = dbus.SessionBus()
-	player_name = os.environ.get('MPRIS_REMOTE_PLAYER', None) or get_player()
-	player_namespace = mpris_prefix + player_name
-	player_obj = bus.get_object(player_namespace, mpris_object)
-	player = dbus.Interface(player_obj, dbus_interface=PLAYER)
-	properties = dbus.Interface(player_obj, dbus_interface=dbus.PROPERTIES_IFACE)
 
 def _path(uri):
 	transport, path = urllib2.splittype(uri)
@@ -36,32 +72,8 @@ def _path(uri):
 		raise ValueError("%r type is not 'file'" % (transport,))
 	return urllib2.unquote(path[2:]).encode('utf-8')
 
-def get_metadata():
-	return properties.GetAll(PLAYER)[METADATA]
-
-def current_file():
-	uri = get_metadata()[URL_PROPERTY]
-	try:
-		return _path(uri)
-	except ValueError:
-		print "Invalid URI: %r" % (uri,)
-		raise
-
-def playing_songs(cb):
-	def playing_uri_changed(source, properties, signature):
-		try:
-			uri = properties[METADATA][URL_PROPERTY]
-		except KeyError:
-			return
-		cb(_path(uri))
-
-	properties.connect_to_signal('PropertiesChanged', playing_uri_changed)
-	cb(current_file())
-	loop = gobject.MainLoop()
-	loop.run()
 
 if __name__ == '__main__':
-	init()
 	def _(s):
 		print repr(s)
-	playing_songs(_)
+	Player().each_track(_)
